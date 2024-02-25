ee80fcbfb0beef0e0396b12e990fb8b33410fcbe
Evelyn Heckman <eyh20@cs.washington.edu>
2024-02-25 15:22:40 -0800

    pp img

---
+++ src/preprocessing_image.py
@@ -0,0 +1,14 @@
+# by Wednesday, preprocessing
+
+# resizing: kion
+  #l1 normalize wherever
+  # 128x128
+  # bilinear interpolate
+
+# converting to grayscale: bt
+  # l1 normalize wherever
+
+# noise filter: evelyn
+  # edge supression: run filter
+  # shrink-grow
+  #l1 normalize if needed
