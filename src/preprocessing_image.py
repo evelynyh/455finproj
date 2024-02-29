dc48ba08a0efb90101bf293ec05c59d1c42e0813
BT Lohitnavy <nlohitna@uw.edu>
2024-02-28 17:56:04 -0800

    created rbg_to_grayscale function + code to loop through folders and save modified images

--- src/preprocessing_image.py
+++ src/preprocessing_image.py
@@ -1,3 +1,7 @@
+from uwimg import *
+import os
+from glob import glob
+
 # by Wednesday, preprocessing
 
 # resizing: kion
@@ -7,6 +11,30 @@
 
 # converting to grayscale: bt
   # l1 normalize wherever
+
+
+def convert_and_save(im_names, newpath, folder, sub_folder):
+    for im_name in im_names:
+        im = load_image(im_name)
+        gray = rgb_to_grayscale(im)
+        save_image(gray, newpath + folder + "\\" + sub_folder + os.path.basename(im_name))
+
+
+path = "C:\\Users\\bt084\\455finproj\\wheres-waldo\\Hey-Waldo\\"
+newpath = "C:\\Users\\bt084\\455finproj\\processed_data\\"
+folders = ["64", "128", "256", "original-images"]
+sub_folders = ["notwaldo", "waldo"]
+for folder in folders:
+    if folder == "original-images":
+        im_names = glob(os.path.join(path + folder + "\\", "*.jpg"))
+
+        convert_and_save(im_names, newpath, folder, sub_folder="")
+
+    else:
+        for sub_folder in sub_folders:
+            im_names = glob(os.path.join(path + folder + "\\" + sub_folder, "*.jpg"))
+            convert_and_save(im_names, newpath, folder, sub_folder + "\\")
+
 
 # noise filter: evelyn
   # edge supression: run filter
