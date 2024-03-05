d855ee2a0f1ef990fae61d07f21cacd3d98ad1bd
BT Lohitnavy <nlohitna@uw.edu>
2024-03-04 19:45:41 -0800

    filter_noise function producing black images

--- src/preprocessing_image.py
+++ src/preprocessing_image.py
@@ -1,6 +1,7 @@
 from uwimg import *
 import os
 from glob import glob
+from pathlib import Path
 
 # resizing: kion
   #l1 normalize wherever
@@ -12,7 +13,7 @@ def convert_and_save(im_names, newpath, folder, sub_folder):
     for im_name in im_names:
         im = load_image(im_name)
         gray = rgb_to_grayscale(im)
-        save_image(gray, newpath + folder + "\\" + sub_folder + os.path.basename(im_name))
+        save_image(gray, newpath + folder + sub_folder + Path(im_name).stem)
 
 # noise filter: evelyn
 # takes a while; only use when necessary
@@ -35,26 +36,31 @@ def filter_noise(img_set, newpath, folder, sub_folder):
             i = i+1
 
         im = bilinear_resize(im, w, h)
-        save_image(im, newpath + folder + "\\" + sub_folder + os.path.basename(x))
+        save_image(im, newpath + folder + sub_folder + os.path.basename(x))
 
 
-path = "C:\\Users\\evely\\CSE 455\\455finproj\\src\\wheres-waldo\\Hey-Waldo\\"
-newpath = "C:\\Users\\evely\\CSE 455\\455finproj\\processed_data\\"
-folders = ["64", "128", "256", "original-images"]
+# path = "C:\\Users\\evely\\CSE 455\\455finproj\\src\\wheres-waldo\\Hey-Waldo\\"
+# newpath = "C:\\Users\\evely\\CSE 455\\455finproj\\processed_data\\"
+path = "C:\\Users\\bt084\\455finproj\\wheres-waldo\\Hey-Waldo\\"
+newpath = "C:\\Users\\bt084\\455finproj\\processed_data\\"
+# folders = ["64", "128", "256", "original-images"]
+# folders = ["64", "64-bw", "64-gray", "128", "128-bw", "128-gray", "256", "256-bw", "256-gray", "original-images"]
+folders = ["original-images"]
 sub_folders = ["notwaldo", "waldo"]
-# for folder in folders:
-#     if folder == "original-images":
-#         im_names = glob(os.path.join(path + folder + "\\", "*.jpg"))
-#
-#         convert_and_save(im_names, newpath, folder, sub_folder="")
-#
-#     else:
-#         for sub_folder in sub_folders:
-#             im_names = glob(os.path.join(path + folder + "\\" + sub_folder, "*.jpg"))
-#             convert_and_save(im_names, newpath, folder, sub_folder + "\\")
-#
+for folder in folders:
+    folder += "\\"
+    if folder == "original-images\\":
+        im_names = glob(os.path.join(path + folder, "*.jpg"))
+        convert_and_save(im_names, newpath, folder, sub_folder="")
+
+    else:
+        for sub_folder in sub_folders:
+            im_names = glob(os.path.join(path + folder + sub_folder, "*.jpg"))
+            convert_and_save(im_names, newpath, folder, sub_folder + "\\")
+
 # folders_gray = ["64-gray", "128-gray", "256-gray"]
 # for folder in folders_gray:
 #     for sub_folder in sub_folders:
-#         im_names = glob(os.path.join(path + folder + "\\" + sub_folder, "*.jpg"))
+#         folder += "\\"
+#         im_names = glob(os.path.join(path + folder + sub_folder, "*.jpg"))
 #         filter_noise(im_names, newpath, folder, sub_folder + "\\")
